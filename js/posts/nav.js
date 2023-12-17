39ab5ebdf42e9b310f53e661d0b1346390e35cb9
SugalAden <112473563+SugalAden@users.noreply.github.com>
2023-12-17 19:15:21 +0100

    Added About page and contact

---
+++ js/posts/nav.js
@@ -0,0 +1,34 @@
+// Function to update the navigation bar based on login status
+function updateNavbar() {
+  const navItemsContainer = document.getElementById("navItems");
+
+  // Get user login status from localStorage
+  const isLoggedIn = !!localStorage.getItem("accessToken");
+
+  // Clear existing items
+  navItemsContainer.innerHTML = "";
+
+  if (isLoggedIn) {
+    // If logged in, show logout link
+    navItemsContainer.innerHTML += `
+        <li class="nav-item">
+          <a class="nav-link text-secondary" id="logoutButton" href="#">Logout</a>
+        </li>
+      `;
+    // Add event listener for logout
+    document.getElementById("logoutButton").addEventListener("click", logout);
+  } else {
+    // If not logged in, show login and signup links
+    navItemsContainer.innerHTML += `
+        <li class="nav-item">
+          <a class="nav-link text-secondary" href="/login.html">Login</a>
+        </li>
+        <li class="nav-item ms-1">
+          <a class="nav-link text-secondary" href="/register.html">Sign Up</a>
+        </li>
+      `;
+  }
+}
+
+// Call updateNavbar on page load
+updateNavbar();
