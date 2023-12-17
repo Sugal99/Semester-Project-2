51ed91624693ed62dddbbabb326754e6ff5f87fb
SugalAden <112473563+SugalAden@users.noreply.github.com>
2023-12-17 18:03:32 +0100

    Added avatar functionlity

--- js/posts/profile.js
+++ js/posts/profile.js
@@ -11,7 +11,6 @@ async function getProfileInfo(url) {
       },
     };
 
-    // Log the API response
     const response = await fetch(url, getData);
     const json = await response.json();
     console.log("API Response:", json);
@@ -23,7 +22,6 @@ async function getProfileInfo(url) {
 
 async function main() {
   try {
-    // Get the profile name from localStorage
     const profileName = localStorage.getItem("user");
 
     if (!profileName) {
@@ -31,14 +29,11 @@ async function main() {
       return;
     }
 
-    // Use the correct endpoint for fetching profile data
     const profileUrl = `${API_BASE_URL}/auction/profiles/${profileName}`;
     const profileData = await getProfileInfo(profileUrl);
 
-    // Retrieve the avatar URL from localStorage
     const savedAvatarUrl = localStorage.getItem("avatar");
 
-    // Update the HTML with the profile information, including the avatar URL
     updateProfileHTML(profileData, savedAvatarUrl);
   } catch (error) {
     console.error("Error in main function:", error);
@@ -48,13 +43,8 @@ async function main() {
 document
   .getElementById("saveAvatarChanges")
   .addEventListener("click", function () {
-    // Retrieve the value entered in the URL field
     var avatarUrl = document.getElementById("avatarUrl").value;
-
-    // Perform actions with the avatar URL, for example, update the profile picture
     updateProfilePicture(avatarUrl);
-
-    // Close the modal
     var modal = new bootstrap.Modal(
       document.getElementById("changeAvatarModal")
     );
@@ -62,32 +52,26 @@ document
   });
 
 function updateProfileHTML(profileData, savedAvatarUrl) {
-  // Assuming you have a <div> with id "userCreditsContainer" in your HTML
   const userCreditsContainer = document.getElementById("profileCredits");
   const testContainer = document.getElementById("test");
   const avatarContainer = document.getElementById("mini-avatar-placeholder");
 
-  // Update the HTML content with the profile information
   userCreditsContainer.textContent = `Credits: ${profileData.credits}`;
 
   testContainer.innerHTML = `
       <p>${profileData.name}</p>
     `;
 
-  // Set the avatar URL from localStorage or use the default avatar URL from the profile data
   const avatarUrl = savedAvatarUrl || profileData.avatar;
-
-  // Set the src attribute directly
   document.getElementById("mini-avatar-placeholder").src = avatarUrl;
 
-  // Save the avatar URL in localStorage
   localStorage.setItem("avatar", avatarUrl);
-
-  // Save credits in localStorage
   localStorage.setItem("credits", profileData.credits);
 }
 
-// Call the main function to start the process
-console.log(localStorage);
+function updateProfilePicture(avatarUrl) {
+  document.getElementById("mini-avatar-placeholder").src = avatarUrl;
+  localStorage.setItem("avatar", avatarUrl);
+}
 
 main();
