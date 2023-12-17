0e3e968694bd4fb288cf62eecdaedc9d7a56b909
SugalAden <112473563+SugalAden@users.noreply.github.com>
2023-12-17 12:45:16 +0100

    Added Create Entry Functionality

---
+++ js/posts/addListing.js
@@ -0,0 +1,71 @@
+async function addListing(url, data) {
+  try {
+    const token = localStorage.getItem("accessToken");
+
+    const postData = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, postData);
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addListingButtonClick() {
+  try {
+    // Get values from input fields
+    const itemName = document.getElementById("itemName").value;
+    const itemDescription = document.getElementById("itemDescription").value;
+    const dateTime = document.getElementById("dateTime").value;
+    const imageUrl = document.getElementById("imageUrl").value;
+
+    // Validate if required fields are not empty
+    if (!itemName || !dateTime) {
+      alert("Please enter the required fields.");
+      return;
+    }
+
+    const data = {
+      title: itemName,
+      description: itemDescription,
+      tags: [],
+      media: imageUrl ? [imageUrl] : [],
+      endsAt: new Date(dateTime).toISOString(),
+    };
+
+    const url = `${API_BASE_URL}/auction/listings`;
+    const token = localStorage.getItem("accessToken"); // Retrieve the token here
+
+    const postData = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, postData);
+    console.log(response);
+
+    const json = await response.json();
+    console.log(json);
+
+    // Add your logic to handle the response as needed
+
+    // Close the modal after successfully adding the listing
+    $("#addListingModal").modal("hide");
+  } catch (error) {
+    console.log(error);
+  }
+}
